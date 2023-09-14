6cdf04eb0623531d3324c706ecdbd0c99b9a2a9c
0m3ga13 <barkaoussama13@gmail.com>
2023-09-14 18:04:05 +0100

    style(test): test

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 
-export const Card = ({ title, name, poster_path }) => {
+export const Card = ({ poster_path }) => {
   return (
     <div className=" block rounded-lg bg-slate-300/25 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700/25">
-      <h5 className=" top-0  p-2 w-full text-center mb-2 text-xl font-medium leading-tight text-black dark:text-white">
-        {title || name}
-      </h5>
       <img
         className=""
         src={"http://image.tmdb.org/t/p/w500" + poster_path}
