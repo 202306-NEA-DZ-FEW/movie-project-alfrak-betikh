c1bb241c6b415e96f6f4c75f2b9a39137ab8199e
0m3ga13 <barkaoussama13@gmail.com>
2023-09-14 12:27:29 +0100

    feat(home page): create home page with trending movie card slider and link to single movie page
    This change involves the creation of a new home page that features a dynamic card slider displaying
    trending movies. Users can interact with these cards to navigate to the respective single movie
    pages for more details.
    
    BREAKING CHANGE: create home page
    
    fix #8

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Card = ({ title, name, poster_path }) => {
+  return (
+    <div className=" block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700/25">
+      <h5 className=" top-0  w-full text-center mb-2 text-xl font-medium leading-tight">
+        {title || name}
+      </h5>
+      <img
+        className=""
+        src={"http://image.tmdb.org/t/p/w500" + poster_path}
+        alt=""
+      ></img>
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,61 @@
-import Image from "next/image";
+import { Card } from "@/components/Card/Card";
+import { fetcher } from "@/utils/API";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Link from "next/link";
 
-export default function Home() {
-  return <></>;
+export async function getStaticProps() {
+  const data = await fetcher("trending/all/day?language=en-US");
+  return {
+    props: {
+      latestMovies: data,
+    },
+  };
+}
+
+export default function Home({ latestMovies }) {
+  // Slick settings
+  const settings = {
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    speed: 2500,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    slide: "div",
+    cssEase: "linear",
+  };
+
+  return (
+    <div className="flex flex-row  mt-24">
+      <div class="flex flex-wrap mx-auto mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
+        <div class="w-full sm:p-4 px-4 mb-6">
+          <h1 class="title-font font-medium text-6xl mb-2 text-gray-200">
+            Welcome to the Ultimate Movie Destination
+          </h1>
+          <p className="leading-relaxed">
+            Explore the exciting world of cinema with us! Discover the latest
+            blockbusters, top-rated classics, and much more. Our curated
+            collection of movies and actors will keep you entertained for hours.
+            Whether you&apos;re a film enthusiast or just looking for a great
+            movie night, we&apos;ve got you covered. Dive into the world of
+            cinema today!
+          </p>
+        </div>
+      </div>
+      <div className="w-96 mx-auto">
+        <Slider {...settings}>
+          {latestMovies.results.map((movie) => (
+            <div key={movie.id}>
+              <Link href={"movies" + movie.id}>
+                <Card {...movie} />
+              </Link>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
 }
