41ba6d1010266ff2d37c7ce186607e175f41f73e
0m3ga13 <barkaoussama13@gmail.com>
2023-09-16 00:02:01 +0100

    feat(movie page): add movie page and conditional fetching

--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -1,5 +1,39 @@
+import MovieCard from "@/components/Cards/MovieCard";
+import { fetcher } from "@/utils/API";
 import React from "react";
 
-export const index = () => {
-  return <div>index</div>;
+export async function getServerSideProps({ query }) {
+  let apiUrl;
+
+  if (query.genre) {
+    // If a genre query is present, fetch movies by genre
+    const genre = query.genre;
+    apiUrl = `discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genre}`;
+  } else {
+    // Default to fetching movies by type
+    const listType = query.type;
+    apiUrl = `movie/${listType}?language=en-US&page=1`;
+  }
+
+  const data = await fetcher(apiUrl);
+
+  return {
+    props: {
+      movies: data,
+    },
+  };
+}
+
+const index = ({ movies }) => {
+  return (
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-wrap flex-row">
+        {movies.results.map((movie) => (
+          <MovieCard {...movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
+
+export default index;
