5c0b39a2e0d750e7a0daa5dd0719ab2f7a564e0f
0m3ga13 <barkaoussama13@gmail.com>
2023-09-15 20:21:59 +0100

    refactor(slick-slider): fix slick slider z-index

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -74,7 +74,7 @@ export default function NavLinks() {
             </h1>
             {link.submenu && (
               <div>
-                <div className="absolute top-20 hidden group-hover:md:block hover:md:block">
+                <div className="absolute z-20 top-20 hidden group-hover:md:block hover:md:block">
                   <div className="py-3">
                     <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
                   </div>
