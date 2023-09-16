542e1101a8288775b5ca2695a7fb9cf049e455c7
hadiadjd <celestialgeek21@gmail.com>
2023-09-16 19:53:44 +0100

    style(navbar): stylize the navbar

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -58,38 +58,26 @@ export default function NavLinks() {
         <div key={link.name}>
           <div className="px-3 text-left md:cursor-pointer group">
             <h1
-              className="py-7 flex text-fuchsia-600 justify-between items-center md:pr-0 pr-5 group"
+              className="relative block text-gray-300 font-bold mt-5 w-fit  after:block after:content-[''] after:absolute after:h-[3px] after:bg-yellow-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left
+              py-7 justify-between items-center md:pr-0 pr-5 group"
               onClick={() => {
                 heading !== link.name ? setHeading(link.name) : setHeading("");
                 setSubHeading("");
               }}
             >
               {link.name}
-              <span className="text-xl md:hidden inline">
-                <ion-icon
-                  name={`${
-                    heading === link.name ? "chevron-up" : "chevron-down"
-                  }`}
-                ></ion-icon>
-              </span>
-              <span className="text-xl md:mt-1 md:ml-2  md:block hidden group-hover:rotate-180 group-hover:-mt-2">
-                <ion-icon name="chevron-down"></ion-icon>
-              </span>
             </h1>
             {link.submenu && (
               <div>
                 <div className="absolute z-20 top-20 hidden group-hover:md:block hover:md:block">
                   <div className="py-3">
-                    <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
+                    <div className="w-4 h-4 left-3 absolute mt-1 bg-gray-900/80 rotate-45"></div>
                   </div>
-                  <div className="bg-white p-5 grid  z-50 grid-cols-3 gap-10">
+                  <div className=" bg-gray-900/80 p-5 grid  z-50 grid-cols-5 gap-5">
                     {link.sublinks.map((sublink, index) => (
                       <div key={index}>
-                        <h1 className="text-lg font-semibold">
-                          {sublink.name}
-                        </h1>
                         <ul>
-                          <li className="text-sm text-gray-600 my-2.5">
+                          <li className="text-sm text-white-600 my-1.5 hover:text-yellow-500">
                             <Link
                               href={{
                                 pathname: "/Movies",
